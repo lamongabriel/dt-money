4021d18de22dc43e759aac46f4c710460f184e92
Gabriel Lamon <gabriel-lamon@outlook.com>
2022-10-21 00:03:42 -0300

    api fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,5 +5,5 @@ export const api = axios.create({
   // change it if you want to use it via node locally
   // http://localhost:3000/api
 
-  baseURL: 'https://lamongabriel-dtmoney.netlify.app'
+  baseURL: 'https://lamongabriel-dtmoney.netlify.app/api'
 })
