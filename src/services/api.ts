b239cdf79d5dda6196aca6623dc694c3ab86a4e8
lamongabriel <gabriel-lamon@outlook.com>
2023-01-12 20:50:15 -0300

    production settings

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // LOCAL URL
-  // change it if you want to use it via node locally
-  // http://localhost:3000/api
-
-  baseURL: 'https://lamongabriel-dtmoney.netlify.app/api'
+  baseURL: process.env.REACT_APP_API_URL as string
 })
